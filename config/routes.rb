3f132444eafed5dad929d7c0fe97e6131719ddf2
LockeZz <LockeZz.PHF@gmail.com>
2021-07-11 17:36:52 +0800

    API sample completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/member-data', to: 'members#show'
 end
